afc911e7bad707e48d69163064fe68aa2810cb5e
Hatun05 <445201400@student.ksu.edu.sa>
2026-03-14 00:31:18 +0300

    logout

--- user_page.php
+++ user_page.php
@@ -7,10 +7,12 @@ if(!isset($_SESSION['userID']) || $_SESSION['userType'] !== 'user'){
     header("Location: login.php?error=unauthorized");
     exit();
 }
+
+// رابط تسجيل الخروج نقطه 12 php
+//<a href="logout.php">sign out</a>
 ?>
 
 <?php
-
 //Test اجرب اذا يشتغل او لا
 
 session_start();
@@ -22,3 +24,5 @@ echo "<br>User Type: ".$_SESSION['userType'];
 <?php
 echo " <br> Welcome user page";
 ?>
+
+
