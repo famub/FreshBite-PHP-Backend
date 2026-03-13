1145e1aefd354b8fa1dc515d30d999289b4ec023
Hatun05 <445201400@student.ksu.edu.sa>
2026-03-13 23:46:23 +0300

    login

---
+++ check_login.php
@@ -0,0 +1,62 @@
+
+<?php
+
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+session_start();
+
+include 'db_connection.php';
+
+$email = $_POST['email'];
+$password = $_POST['password'];
+
+$sql_blocked = "SELECT * FROM BlockedUser WHERE emailAddress='$email'";
+$result_blocked = mysqli_query($conn,$sql_blocked);
+
+if(mysqli_num_rows($result_blocked)>0){
+
+header("Location: login.php?error=blocked");
+exit();
+
+}
+
+$sql_user = "SELECT * FROM User WHERE emailAddress='$email'";
+$result_user = mysqli_query($conn,$sql_user);
+
+if(mysqli_num_rows($result_user)==1){
+
+$user = mysqli_fetch_assoc($result_user);
+
+if(password_verify($password,$user['password'])){
+
+$_SESSION['userID'] = $user['id'];
+$_SESSION['userType'] = $user['userType'];
+
+if($user['userType']=="admin"){
+
+header("Location: admin_page.php");
+
+}else{
+
+header("Location: user_page.php");
+
+}
+
+exit();
+
+}else{
+
+header("Location: login.php?error=password");
+exit();
+
+}
+
+}else{
+
+header("Location: login.php?error=email");
+exit();
+
+}
+
+?>
--- login.php
+++ login.php
@@ -1,51 +1,3 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-if(session_status() == PHP_SESSION_NONE){
-    session_start();
-}
-
-include 'db_connection.php';
-
-$error = "";
-
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-    $email = $_POST['email'] ?? '';
-    $password = $_POST['password'] ?? '';
-
-    $sql_blocked = "SELECT * FROM BlockedUser WHERE emailAddress='$email'";
-    $result_blocked = mysqli_query($conn, $sql_blocked);
-
-    if(mysqli_num_rows($result_blocked) > 0){
-        $error = "This account is blocked!";
-    } else {
-        $sql_user = "SELECT * FROM User WHERE emailAddress='$email'";
-        $result_user = mysqli_query($conn, $sql_user);
-
-        if(mysqli_num_rows($result_user) == 1){
-            $user = mysqli_fetch_assoc($result_user);
-
-            if(password_verify($password, $user['password'])){
-                $_SESSION['userID'] = $user['id'];
-                $_SESSION['userType'] = $user['userType'];
-
-                if($user['userType'] === 'admin'){
-                    header("Location: admin_page.php");
-                } else {
-                    header("Location: user_page.php");
-                }
-                exit();
-            } else {
-                $error = "Incorrect password!";
-            }
-        } else {
-            $error = "Email not found!";
-        }
-    }
-}
-?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -71,16 +23,30 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
                 <p class="signup-text">
                     New User? <a href="SignUp.php" class="signup-link">Sign-up</a>
                 </p>
-            </div>
-        </div>
-        <div class="right-side"></div>
-    </div>
+            
 
     <?php
-    // إذا كان هناك خطأ، يظهر alert
-    if(!empty($error)){
-        echo "<script>alert('$error');</script>";
-    }
-    ?>
+        if(isset($_GET['error'])){
+
+        if($_GET['error']=="blocked"){
+        echo "<script>alert('This account is blocked!');</script>";
+        }
+
+        if($_GET['error']=="password"){
+        echo "<script>alert('Incorrect password!');</script>";
+        }
+
+        if($_GET['error']=="email"){
+        echo "<script>alert('Email not found!');</script>";
+        }
+
+        }
+     ?>
+
+    </div>
+        </div>
+
+        <div class="right-side"></div>
+    </div>
 </body>
 </html>
