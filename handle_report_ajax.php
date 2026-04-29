6d256c6905fc8dc13f70b817e79614f36f51b2f9
shahd351 <shahdd.kh@gmail.com>
2026-04-29 14:15:57 +0300

    3
    3

---
+++ handle_report_ajax.php
@@ -0,0 +1,55 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['userID']) || $_SESSION['userType'] !== 'admin') {
+    echo "false";
+    exit();
+}
+
+require_once 'db_connection.php';
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $report_id = intval($_POST['report_id']);
+    $owner_id = intval($_POST['owner_id']);
+    $action = $_POST['action'];
+
+    if ($action === 'block') {
+        $get_recipes = "SELECT recipePhoto FROM recipe WHERE userID = $owner_id";
+        $recipes_result = mysqli_query($conn, $get_recipes);
+        while ($recipe = mysqli_fetch_assoc($recipes_result)) {
+            $photo = $recipe['recipePhoto'];
+            if (!empty($photo) && file_exists("images/" . $photo)) {
+                unlink("images/" . $photo);
+            }
+        }
+
+        $query = "SELECT firstName, lastName, emailAddress, chefPhoto FROM user WHERE id = $owner_id";
+        $result = mysqli_query($conn, $query);
+        $user = mysqli_fetch_assoc($result);
+
+        if ($user) {
+            if (!empty($user['chefPhoto']) && file_exists("images/" . $user['chefPhoto'])) {
+                unlink("images/" . $user['chefPhoto']);
+            }
+
+            $insert = "INSERT INTO blockeduser (firstName, lastName, emailAddress) 
+                       VALUES ('{$user['firstName']}', '{$user['lastName']}', '{$user['emailAddress']}')";
+            mysqli_query($conn, $insert);
+
+            $delete = "DELETE FROM user WHERE id = $owner_id";
+            mysqli_query($conn, $delete);
+        }
+    } elseif ($action === 'dismiss') {
+        $get_recipe = "SELECT recipeID FROM report WHERE id = $report_id";
+        $recipe_result = mysqli_query($conn, $get_recipe);
+        $recipe_row = mysqli_fetch_assoc($recipe_result);
+        $recipe_id = $recipe_row['recipeID'];
+
+        $delete_all_reports = "DELETE FROM report WHERE recipeID = $recipe_id";
+        mysqli_query($conn, $delete_all_reports);
+    }
+
+    echo "true";
+    exit();
+}
+?>
